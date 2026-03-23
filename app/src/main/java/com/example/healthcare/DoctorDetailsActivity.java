22b3be064b1d5da3cddcb71b649bb827ddbe98c7
prakhar <prakhargoyal04@gmail.com>
2026-03-23 15:39:35 +0530

    fp

--- app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
@@ -3,7 +3,10 @@ package com.example.healthcare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,9 +15,61 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DoctorDetailsActivity extends AppCompatActivity {
+    private String[][] doctor_details1 =
+            {
+                    {"Doctor Name : Ajit Saste", "Hospital Address : Pimpri", "Exp : 5yrs","Mobile no : 9898989898", "600"},
+                    {"Doctor Name : Prasad Pawar", "Hospital Address : Nindi", "Exp : 15yrs","Mobile no : 7898989898", "900"},
+                    {"Doctor Name : Himanshu Kumar", "Hospital Address : Pune", "Exp : 8yrs","Mobile no : 8898989898", "300"},
+                    {"Doctor Name : Deepak Deshmukh", "Hospital Address : Rampur", "Exp : 6yrs","Mobile no : 9898000000", "500"},
+                    {"Doctor Name : Ashok Panda", "Hospital Address : Moradabad", "Exp : 7yrs","Mobile no : 7798989898", "800"}
+            };
+
+    private String[][] doctor_details2 =
+            {
+                    {"Doctor Name : Prakhar Goyal", "Hospital Address : Moradabad", "Exp : 10yrs","Mobile no : 9898989898", "600"},
+                    {"Doctor Name : Dipika Rohila", "Hospital Address : Bareilly", "Exp : 15yrs","Mobile no : 7898989898", "900"},
+                    {"Doctor Name : Himanshu Kumar", "Hospital Address : Rampur", "Exp : 8yrs","Mobile no : 8898989898", "300"},
+                    {"Doctor Name : Deepak Deshmukh", "Hospital Address : Rampur", "Exp : 6yrs","Mobile no : 9898000000", "500"},
+                    {"Doctor Name : Ashok Panda", "Hospital Address : Moradabad", "Exp : 7yrs","Mobile no : 7798989898", "800"}
+            };
+
+    private String[][] doctor_details3 =
+            {
+                    {"Doctor Name : Ajit Saste", "Hospital Address : Pimpri", "Exp : 5yrs","Mobile no : 9898989898", "600"},
+                    {"Doctor Name : Prasad Pawar", "Hospital Address : Nindi", "Exp : 15yrs","Mobile no : 7898989898", "900"},
+                    {"Doctor Name : Himanshu Kumar", "Hospital Address : Pune", "Exp : 8yrs","Mobile no : 8898989898", "300"},
+                    {"Doctor Name : Deepak Deshmukh", "Hospital Address : Rampur", "Exp : 6yrs","Mobile no : 9898000000", "500"},
+                    {"Doctor Name : Ashok Panda", "Hospital Address : Moradabad", "Exp : 7yrs","Mobile no : 7798989898", "800"}
+            };
+
+    private String[][] doctor_details4 =
+            {
+                    {"Doctor Name : Prashant Chauhan", "Hospital Address : Pimpri", "Exp : 5yrs","Mobile no : 9898989898", "600"},
+                    {"Doctor Name : Anmol", "Hospital Address : Nindi", "Exp : 15yrs","Mobile no : 7898989898", "900"},
+                    {"Doctor Name : Himanshu Kumar", "Hospital Address : Pune", "Exp : 8yrs","Mobile no : 8898989898", "300"},
+                    {"Doctor Name : Deepak Deshmukh", "Hospital Address : Rampur", "Exp : 6yrs","Mobile no : 9898000000", "500"},
+                    {"Doctor Name : Aman", "Hospital Address : Moradabad", "Exp : 7yrs","Mobile no : 7798989898", "800"}
+            };
+
+    private String[][] doctor_details5 =
+            {
+                    {"Doctor Name : Arjun Kumar", "Hospital Address : Pimpri", "Exp : 5yrs","Mobile no : 9898989898", "600"},
+                    {"Doctor Name : Prasad Pawar", "Hospital Address : Nindi", "Exp : 15yrs","Mobile no : 7898989898", "900"},
+                    {"Doctor Name : Himanshu Kumar", "Hospital Address : Pune", "Exp : 8yrs","Mobile no : 8898989898", "700"},
+                    {"Doctor Name : Naman Goyal", "Hospital Address : Rampur", "Exp : 6yrs","Mobile no : 9898000000", "500"},
+                    {"Doctor Name : Ashok Panda", "Hospital Address : Moradabad", "Exp : 7yrs","Mobile no : 7798989898", "800"}
+            };
     TextView tv;
     Button btn;
+    String[][] doctor_details = {};
+    HashMap<String,String> item;
+    ArrayList list;
+    SimpleAdapter sa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +83,18 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         String title = it.getStringExtra("title");
         tv.setText(title);
 
+        if (title.compareTo("Family Physician") == 0)
+            doctor_details = doctor_details1;
+        else if (title.compareTo("Dietician") == 0)
+            doctor_details = doctor_details2;
+        else if (title.compareTo("Dentist") == 0)
+            doctor_details = doctor_details3;
+        else if (title.compareTo("Surgeon") == 0)
+            doctor_details = doctor_details4;
+        else
+            doctor_details = doctor_details5;
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,5 +102,41 @@ public class DoctorDetailsActivity extends AppCompatActivity {
             }
         });
 
+        list = new ArrayList();
+        for (int i = 0; i < doctor_details.length; i++) {
+            item = new HashMap<String, String>();
+            item.put("line1", doctor_details[i][0]);
+            item.put("line2", doctor_details[i][1]);
+            item.put("line3", doctor_details[i][2]);
+            item.put("line4", doctor_details[i][3]);
+            item.put("line5", "Cons Fees : " + doctor_details[i][4] + "/-");
+            list.add(item);
+
+        }
+        sa = new SimpleAdapter(this, list,
+                R.layout.multi_lines,
+                new String[]{"line1", "line2", "line3", "line4", "line5"},
+                new int[]{R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e}
+        );
+
+        ListView lst = findViewById(R.id.listViewDD);
+        lst.setAdapter(sa);
+
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it = new Intent(DoctorDetailsActivity.this, BookAppointmentActivity.class);
+                it.putExtra("text1",title);
+                it.putExtra("text2",doctor_details[i][0]);
+                it.putExtra("text3",doctor_details[i][1]);
+                it.putExtra("text4",doctor_details[i][3]);
+                it.putExtra("text5",doctor_details[i][4]);
+                startActivity(it);
+
+
+
+            }
+
+        });
     }
 }
